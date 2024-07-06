62bb5825d0aeb793b49b99f6b027313eab4ea2b3
kyra <skrimmail@proton.me>
2024-07-06 13:35:44 +0200

    Update dualsensectl.sh

--- dualsensectl.sh
+++ dualsensectl.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 command_output=$(dualsensectl battery 2>/dev/null)
-echo "$command_output"
 
+case $command_output in
+	*" charging"*)
+		output=$(dualsensectl battery | tr -d -c 0-9)
+		echo "$output% charging";;
+	*" discharging"*)
+		output=$(dualsensectl battery | tr -d -c 0-9)
+		echo "$output%";;
+	**)
+		echo "$command_output";;
+esac
